1a79f78b26e2c9e75d1642c1303fa68f41f77f33
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-03-10 05:04:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+var dwarves = ['Doc', 'Dopey', 'Bashful', 'Grumpy']
+
 function dwarfRollCall(dwarves) {
+  var string = []
+  for(var i=0; i<dwarves.length; i++){
+    string.push((i+1) + ". " + dwarves[i] +" ")
+  }
+  return string.join("")
 }
 
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+
 function summonCaptainPlanet(planeteerCalls){
+  var array = []
+  for(var i=0; i<planeteerCalls.length; i++){
+    array.push(planeteerCalls[i].toUpperCase() + "!")
+  }
+  return array
 }
 
 function longPlaneteerCalls(words) {
+  for (var i=0; i<words.length; i++)
+  if (words[i].length>4){
+    return true}
+    else {
+      return false}
 }
 
 function findTheCheese (foods) {
+for(var i =0; i < foods.length; i++) {
+if(foods[i] === "cheddar" ){
+return foods[i];
+  } 
+}
+if (foods !== "cheddar") {
+return "no cheese!";
+  } 
 }
